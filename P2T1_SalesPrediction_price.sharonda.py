1d5236596a511a3949ff39873c2cc3405751eec5
prices9250 <38636333+prices9250@users.noreply.github.com>
2018-04-22 19:04:04 -0400

    Add files via upload
    First submission-P2T1_SalesPrediction

---
+++ P2T1_SalesPrediction_price.sharonda.py
@@ -0,0 +1,18 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Sharonda Price
+#4/22/2018
+#
+
+
+#Determine the total_sales.
+
+total_sales = float(input( "Enter the amount of sales:"))
+
+#calculate annual profit as 23%(.0.23) of total sales.
+
+annualProfit = total_sales * 0.23
+
+#Display the annual profit.
+
+print("Your annual profit is ",annualProfit)
